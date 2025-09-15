5c0767bfdfaa3faa664338589a566d7e79cf24e1
parmstro <parmstro@redhat.com>
2025-09-15 19:53:27 +0000

    add build replicas (tested with new, rebuilt and existing)

--- build_idm_replicas.sh
+++ build_idm_replicas.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i /rhis/vars/external_inventory/inventory -e "vault_dir=/rhis/vars/vault" -u ansiblerunner --ask-pass --ask-vault-pass /rhis/rhis-builder-idm/replicas_main.yml
+ansible-playbook -i /rhis/vars/external_inventory/inventory -e "vault_dir=/rhis/vars/vault" -u ansiblerunner --ask-pass --ask-vault-pass --limit=idm_replicas /rhis/rhis-builder-idm/replicas_main.yml

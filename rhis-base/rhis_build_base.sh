cd3fa27ad3b36a665a8aaed42a8cbc46f2d3b139
parmstro <parmstro@redhat.com>
2025-10-11 21:54:18 +0000

    new build scripts manage versioning manage ansiblever pushed to quay.io

--- rhis-base/rhis_build_base.sh
+++ rhis-base/rhis_build_base.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-version=$(<version.txt)
+
+# default to AAP 2.4
 ansiblever="2.4"
+version_file="./version24.txt"
+version_mode="revision"
+
 nocache="false"
 buildargs=""
 ansiblecfg="/etc/ansible/ansible.cfg"
+push_registry="quay.io"
+push_registry_login=""
+push_registry_token=""
 
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
@@ -19,40 +26,135 @@ while [[ "$#" -gt 0 ]]; do
             ansiblecfg="$2"
             shift
             ;;
+        -r|--push-registry)
+            push_registry="$2"
+            shift
+            ;;
+        -u|--push-registry-login)
+            push_registry_login="$2"
+            shift
+            ;;
+        -t|--push-registry-token)
+            push_registry_token="$2"
+            shift
+            ;; 
+        -m|--version-mode)
+            version_mode="$2"
+            shift
+            ;; 
         *)
             echo "Unknown option: $1"
+            echo "Usage: rhis_build_base.sh [options]"
+            echo "Options:"
+            echo "    --no-cache - rebuild container from scatch"
+            echo "    --ansible-ver - specify the AAP API version - one of '2.4' (default) or '2.5'"
+            echo "    --ansible-config path_spec - provide the path specification to the ansible.cfg file (default: /etc/ansible/ansible.cfg)"
+            echo "    --push-registry - the name of the remote registry to push the final image to (default: quay.io)"
+            echo "    --push-registry-login - the login for the push registry (e.g. mybot)"
+            echo "    --push-registry-token - the authentication token for the push registry"
+            echo "    --version-mode - increment major, minor, or revision version of the build"
             exit 1
             ;;
     esac
     shift # Shift past the option
 done
 
-sudo dnf -y install ansible-core podman 
-podman login registry.redhat.io
+build_container() {
+  echo "Starting build of rhis-base container version: $version for AAP version: $ansiblever"
+  echo
+  echo "Ensuring ansible and podman requirements are installed..."
+  sudo dnf -y install ansible-core podman
 
-rm -f sources/*
-cp $ansiblecfg sources/ansible.cfg
-cp ansible.cfg.clean sources/ansible.cfg.clean
-cp requirements.yml sources/requirements.yml
-cp requirements.txt sources/requirements.txt
-cp README.md sources/README.md
+  echo "Using registry.redhat.io as the pull registry"
+  podman login registry.redhat.io
 
-echo
-echo "Running 'podman build' with the following parameters:"
-echo
-echo "ansible-ver: $ansiblever"
-echo "no-cache: $nocache"
-echo
+  if [[ -n "$push_registry" && -n "$push_registry_login" && -n "$push_registry_token" ]]; then
+    echo "Using $push_registry as the push registry"
+    podman login $push_registry -u $push_registry_login -p $push_registry_token
+  else
+    echo "push_registry parameters not defined. Continuing with local build."
+  fi
 
-if [[ $ansiblever == "2.5" ]]; then
-  buildargs="--build-arg ANSIBLE_VER=2.5"
+  rm -f sources/*
+  cp $ansiblecfg sources/ansible.cfg
+  cp ansible.cfg.clean sources/ansible.cfg.clean
+  cp requirements.yml sources/requirements.yml
+  cp requirements.txt sources/requirements.txt
+  cp README.md sources/README.md
+
+  echo
+  echo "Running 'podman build' with the following parameters:"
+  echo
+  echo "ansible-ver: $ansiblever"
+  echo "no-cache: $nocache"
+  echo
+
+  if [[ $ansiblever == "2.5" ]]; then
+    buildargs="--build-arg ANSIBLE_VER=2.5 --build-arg OS_VER=9 --build-arg RHIS_VER=$version"
+  else
+    buildargs="--build-arg ANSIBLE_VER=2.4 --build-arg OS_VER=9 --build-arg RHIS_VER=$version"
+  fi
+
+  if [[ $nocache == "true" ]]; then
+    buildargs+=" --no-cache"
+  fi
+
+  podman build $buildargs --squash -t rhis-base-9-$ansiblever:$version .
+  podman tag localhost/rhis-base-9-$ansiblever:$version rhis-base-9-$ansiblever:latest
+
+  if [[ -n "$push_registry" && -n "$push_registry_login" && -n "$push_registry_token" ]]; then
+    podman tag localhost/rhis-base-9-$ansiblever:$version quay.io/parmstro/rhis-base-9-$ansiblever:$version
+    podman tag localhost/rhis-base-9-$ansiblever:$version quay.io/parmstro/rhis-base-9-$ansiblever:latest
+    podman push quay.io/parmstro/rhis-base-9-$ansiblever:$version
+    podman push quay.io/parmstro/rhis-base-9-$ansiblever:latest
+  fi
+}
+
+increment_version() {
+  if [[ $ansiblever == "2.5" ]]; then
+    version_file="./version25.txt"
+  else
+    version_file="./version24.txt"
+  fi
+  current_version=$(cat $version_file)
+  
+  IFS='.' read -r major minor revision <<< "$current_version"
+  case "$1" in
+    "major")
+        major=$((major + 1))
+        minor=0
+        revision=0
+        ;;
+    "minor")
+        minor=$((minor + 1))
+        revision=0
+        ;;
+    "revision")
+        revision=$((revision + 1))
+        ;;
+    *)
+        echo "Invalid mode"
+        exit 1
+  esac
+
+  # Create the new version string
+  new_version="$major.$minor.$revision"
+  echo "${new_version}"
+}
+
+update_version() {
+  echo $version > $version_file
+}
+
+version=$(increment_version "$version_mode")
+# Run main commands
+build_container
+
+# Check the exit status of the main commands
+if [[ $? -eq 0 ]]; then
+    # If build was successful, increment the revision
+    echo "Successfully built $version - Updating version file."
+    update_version
 else
-  buildargs="--build-arg ANSIBLE_VER=2.4"
+    echo "One or more build commands failed. Version file not updated."
 fi
-
-if [[ $nocache == "true" ]]; then
-  buildargs+=" --no-cache"
-fi
-
-podman build $buildargs --squash -t rhis-base-9-$ansiblever:$version .
-podman tag localhost/rhis-base-9-$ansiblever:$version rhis-base-9-$ansiblever:latest
--- rhis-provisioner/rhis_build_provisioner.sh
+++ rhis-provisioner/rhis_build_provisioner.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-version=$(<version.txt)
+
+# default to AAP 2.4
 ansiblever="2.4"
+version_file="./version24.txt"
+version_mode="revision"
+base_version_file="../rhis-base/version24.txt" 
+
 nocache="false"
 buildargs=""
 ansiblecfg="/etc/ansible/ansible.cfg"
-registry="quay.io"
-registry_login=""
-registry_token=""
+push_registry="quay.io"
+push_registry_login=""
+push_registry_token=""
 
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
@@ -22,62 +27,155 @@ while [[ "$#" -gt 0 ]]; do
             ansiblecfg="$2"
             shift
             ;;
-        -r|--registry)
-            registry="$2"
+        -r|--push-registry)
+            push_registry="$2"
             shift
             ;;
-        -u|--registry_login)
-            registry_login="$2"
+        -u|--push-registry-login)
+            push_registry_login="$2"
             shift
             ;;
-        -t|--registry_token)
-            registry_token="$2"
+        -t|--push-registry-token)
+            push_registry_token="$2"
+            shift
+            ;; 
+        -m|--version-mode)
+            version_mode="$2"
             shift
             ;; 
         *)
             echo "Unknown option: $1"
+            echo "Usage: rhis_build_provisioner.sh [options]"
+            echo "Options:"
+            echo "    --no-cache - rebuild container from scatch"
+            echo "    --ansible-ver - specify the AAP API version - one of '2.4' (default) or '2.5'"
+            echo "    --ansible-config path_spec - provide the path specification to the ansible.cfg file (default: /etc/ansible/ansible.cfg)"
+            echo "    --push-registry - the name of the remote registry to push the final image to (default: quay.io)"
+            echo "    --push-registry-login - the login for the push registry (e.g. mybot)"
+            echo "    --push-registry-token - the authentication token for the push registry"
+            echo "    --version-mode - increment major, minor, or revision version of the build"
             exit 1
             ;;
     esac
     shift # Shift past the option
 done
 
-sudo dnf -y install ansible-core podman 
-podman login $registry -u $registry_login -p $registry_token
+build_container() {
+  echo "Starting build of rhis-provisioner container version: $version for AAP version: $ansiblever"
+  echo
+  echo "Ensuring ansible and podman requirements are installed..."
+  sudo dnf -y install ansible-core podman
 
-cp $ansiblecfg sources/ansible.cfg
-cp ansible.cfg.clean sources/ansible.cfg.clean
-cp add_softlinks.yml sources/add_softlinks.yml
-cp rhis-builder_sample_commands.txt sources/rhis-builder_sample_commands.txt 
-cp build_idm_primary.sh sources/build_idm_primary.sh
-cp build_idm_replicas.sh sources/build_idm_replicas.sh
-cp build_sat_primary_connected.sh sources/build_sat_primary_connected.sh
-cp build_test_hosts.sh sources/build_test_hosts.sh
-cp destroy_test_hosts.sh sources/destroy_test_hosts.sh
-cp build_aap_controller24.sh sources/build_aap_controller24.sh
-cp build_aap_hub24.sh sources/build_aap_hub24.sh
-cp README.md sources/README.md
+  if [[ -n "$push_registry" && -n "$push_registry_login" && -n "$push_registry_token" ]]; then
+    echo "Using $push_registry as the pull and push registry. Logging in."
+    podman login $push_registry -u $push_registry_login -p $push_registry_token
+  else
+    echo "push_registry parameters not defined. Continuing with local build."
+  fi
 
-cp ipareplica_test_patch.py sources/ipareplica_test_patch.py
+  echo "Configure sources"
+  cp $ansiblecfg sources/ansible.cfg
+  cp ansible.cfg.clean sources/ansible.cfg.clean
+  cp add_softlinks.yml sources/add_softlinks.yml
+  cp rhis-builder_sample_commands.txt sources/rhis-builder_sample_commands.txt 
+  cp build_idm_primary.sh sources/build_idm_primary.sh
+  cp build_idm_replicas.sh sources/build_idm_replicas.sh
+  cp build_sat_primary_connected.sh sources/build_sat_primary_connected.sh
+  cp build_test_hosts.sh sources/build_test_hosts.sh
+  cp destroy_test_hosts.sh sources/destroy_test_hosts.sh
+  cp build_aap_controller24.sh sources/build_aap_controller24.sh
+  cp build_aap_hub24.sh sources/build_aap_hub24.sh
+  cp README.md sources/README.md
 
-echo
-echo "Running 'podman build' with the following parameters:"
-echo
-echo "ansible-ver: $ansiblever"
-echo "no-cache: $nocache"
-echo
+  cp ipareplica_test_patch.py sources/ipareplica_test_patch.py
 
-if [[ $ansiblever == "2.5" ]]; then
-  buildargs="--build-arg ANSIBLE_VER=2.5 --build-arg OS_VER=9 --build-arg RHIS_VER=$version"
+  echo
+  echo "Running 'podman build' with the following parameters:"
+  echo
+  echo "ansible-ver: $ansiblever"
+  echo "no-cache: $nocache"
+  echo
+
+  if [[ $ansiblever == "2.5" ]]; then
+    buildargs="--build-arg ANSIBLE_VER=2.5 --build-arg OS_VER=9 --build-arg RHIS_BASE_VER=$base_version --build-arg RHIS_VER=$version"
+  else
+    buildargs="--build-arg ANSIBLE_VER=2.4 --build-arg OS_VER=9 --build-arg RHIS_BASE_VER=$base_version --build-arg RHIS_VER=$version"
+  fi
+
+  if [[ $nocache == "true" ]]; then
+    buildargs+=" --no-cache"
+  fi
+
+  podman build $buildargs -t rhis-provisioner-9-$ansiblever:$version .
+  podman tag localhost/rhis-provisioner-9-$ansiblever:$version rhis-provisioner-9-$ansiblever:latest
+
+  if [[ $push_registry && $push_registry_login && $push_registry_token ]]; then
+    podman tag localhost/rhis-provisioner-9-$ansiblever:$version quay.io/parmstro/rhis-provisioner-9-$ansiblever:$version
+    podman tag localhost/rhis-provisioner-9-$ansiblever:$version quay.io/parmstro/rhis-provisioner-9-$ansiblever:latest
+    podman push quay.io/parmstro/rhis-provisioner-9-$ansiblever:$version
+    podman push quay.io/parmstro/rhis-provisioner-9-$ansiblever:latest
+  fi
+}
+
+get_base_version() {
+  if [[ $ansiblever == "2.5" ]]; then
+    base_version_file="../rhis-base/version25.txt" 
+  else
+    base_version_file="../rhis-base/version24.txt" 
+  fi
+  current_base_version=$(cat $base_version_file)
+  echo "${current_base_version}"
+}
+
+increment_version() {
+  if [[ $ansiblever == "2.5" ]]; then
+    version_file="./version25.txt"
+    base_version_file="../rhis-base/version25.txt" 
+  else
+    version_file="./version24.txt"
+    base_version_file="../rhis-base/version24.txt" 
+  fi
+  current_version=$(cat $version_file)
+  base_vserion=$(cat $base_version_file)
+
+  IFS='.' read -r major minor revision <<< "$current_version"
+  case "$1" in
+    "major")
+        major=$((major + 1))
+        minor=0
+        revision=0
+        ;;
+    "minor")
+        minor=$((minor + 1))
+        revision=0
+        ;;
+    "revision")
+        revision=$((revision + 1))
+        ;;
+    *)
+        echo "Invalid mode"
+        exit 1
+  esac
+
+  # Create the new version string
+  new_version="$major.$minor.$revision"
+  echo "${new_version}"
+}
+
+update_version() {
+  echo $version > $version_file
+}
+
+base_version=$(get_base_version)
+version=$(increment_version "$version_mode")
+# Run main commands
+build_container
+
+# Check the exit status of the main commands
+if [[ $? -eq 0 ]]; then
+    # If build was successful, increment the revision
+    echo "Successfully built $version - Updating version file."
+    update_version
 else
-  buildargs="--build-arg ANSIBLE_VER=2.4 --build-arg OS_VER=9 --build-arg RHIS_VER=$version"
+    echo "One or more build commands failed. Version file not updated."
 fi
-
-if [[ $nocache == "true" ]]; then
-  buildargs+=" --no-cache"
-fi
-
-podman build $buildargs -t rhis-provisioner-9-$ansiblever:$version .
-podman tag localhost/rhis-provisioner-9-$ansiblever:$version rhis-provisioner-9-$ansiblever:latest
-podman tag localhost/rhis-provisioner-9-$ansiblever:$version quay.io/parmstro/rhis-provisioner-9-$ansiblever:$version
-podman push quay.io/parmstro/rhis-provisioner-9-$ansiblever:$version

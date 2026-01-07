9137dcddc51a01bd3a099db15944659e0dd86225
parmstro <parmstro@redhat.com>
2026-01-07 18:43:56 +0000

    adding app_platform_post script

---
+++ rhis-provisioner/apply_aap_platform_post.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Using rhis-builder-aap to run post configuration using the default inventory"
+
+cd /rhis/rhis-builder-aap/
+
+ansible-playbook --inventory /rhis/vars/external_inventory/inventory \
+                 --user ansiblerunner \
+                 --ask-pass \
+                 --ask-vault-pass \
+                 --extra-vars "vault_dir=/rhis/vars/vault" \
+                 --limit=platform_installer \
+                 --extra-vars "role_name=platform_post" \
+                 run_role.yml

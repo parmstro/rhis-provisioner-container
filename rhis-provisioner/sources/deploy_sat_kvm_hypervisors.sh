49fa730ea778df4c5054beb65b8db55d9c5d84d6
parmstro <parmstro@redhat.com>
2026-03-17 22:31:49 +0000

    See CHANGELOG.txt

--- rhis-provisioner/sources/deploy_sat_kvm_hypervisors.sh
+++ rhis-provisioner/sources/deploy_sat_kvm_hypervisors.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "Using Satellite to build Host Group test hosts defined in list hostgroup_test_hosts_rhel9 defined in group_vars/provisioner/ directory"
-echo "WARNING: This creates a large number of VMs in the default configuration"
-echo "CTRL+C to exit"
+echo "Using Satellite to deploy satellite capsule hosts defined in list kvm_hypervisor_hosts defined in group_vars/provisioner/capsule_hosts.yml"
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color/Normal
 printf "${GREEN}Start Time: %(%T)T${NC}\n" -1
@@ -24,20 +22,14 @@ while [[ "$#" -gt 0 ]]; do
     shift # Shift past the option
 done
 
-sleep 15
-
-echo "Beginning builds..."
-
 ansible-playbook --inventory /rhis/vars/external_inventory/inventory \
                  --user $sshuser \
                  --ask-pass \
                  --ask-vault-password \
                  --extra-vars "vault_dir=/rhis/vars/vault" \
-                 --extra-vars "platform_hosts={{ hostgroup_test_hosts_rhel9 }}" \
+                 --extra-vars "platform_hosts={{ kvm_hypervisor_hosts }}" \
                  --limit=provisioner \
                  rhis_build_from_provisioner.yml
-
-echo "Builds Complete."
 
 duration=$SECONDS
 printf "\n${GREEN}End Time: %(%T)T${NC}\n" -1
